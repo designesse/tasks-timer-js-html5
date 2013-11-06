af54fdb4efd4fcf8ba45ca08204821c41ea285ce
designesse <malinar@yahoo.com>
2013-11-05 18:03:51 -0700

    added HTML5 localstorage implementation.

--- time-manager.js
+++ time-manager.js
@@ -3,25 +3,40 @@ window.onload = function(){
     var timerIntervalId;
     var tasks = [];
 
-    var deleteTask = function(deleteButton) {
-        var task = deleteButton.parentNode;
-        clearInterval(timerIntervalId);
-        task.remove();
+    var supportsHtml5Storage = function () {
+        try {
+            return 'localStorage' in window && window['localStorage'] !== null;
+        } catch (e) {
+            alert(" HTML5 storage is not supported ");
+        }
     }
 
-    var incrementTimer = function(timeButton, task, initialTime, startTime) {
-        var elapsedTime = new Date();
-        var elapsedTimeValue = timeButton.nextSibling.firstChild;
-        elapsedTime = Date.now() - startTime + initialTime;
+    var deleteErrorMessage = function () {
+        document.getElementById("error-message").textContent = '';
+    }
 
-        // Update the tasks variable.
-        tasks[task] = elapsedTime;
+    var displayErrorMessage = function (message) {
+        document.getElementById("error-message").textContent = message;
+    }
 
+    var isNoTaskStarted = function () {
+        var timeButtons = document.getElementsByClassName("control-time");
+        var isStarted = true;
+        for (var i=0; timeButtons[i]; i++) {
+            if (timeButtons[i].innerHTML == 'STOP') {
+                isStarted = false;
+                break;
+            }
+        }
+        return isStarted;
+    }
+
+    var convertToClockTime = function (time) {
         var hours = 0;
         var mins = 0;
         var secs = 0;
-        if (elapsedTime > 999) {
-            secs = elapsedTime / 1000;
+        if (time > 999) {
+            secs = time / 1000;
             if (secs > 59) {
                 mins = secs / 60;
                 secs = secs % 60;
@@ -31,9 +46,83 @@ window.onload = function(){
                 }
             }
         }
+        var clockTime = ('0' + parseInt(hours)).slice(-2) + ":" + ('0' + parseInt(mins)).slice(-2) + ":" + ('0' + parseInt(secs)).slice(-2);
+        return clockTime;
+    }
 
-        var time = ('0' + parseInt(hours)).slice(-2) + ":" + ('0' + parseInt(mins)).slice(-2) + ":" + ('0' + parseInt(secs)).slice(-2);
-        elapsedTimeValue.nodeValue = time;
+    var createListItem = function(newTask, time) {
+        time = typeof time == 'undefined'? 0 : time;
+        // Create a list element for the new task.
+        var li = document.createElement("li");
+        var deleteButton = document.createElement("button");
+        deleteButton.className = "delete-task";
+        deleteButton.innerHTML = 'x';
+        deleteButton.onclick = function(){deleteTask(this);};
+        li.appendChild(deleteButton);
+        var taskName = document.createElement("div");
+        taskName.className = "task-name";
+        taskName.innerHTML = newTask;
+        li.appendChild(taskName);
+        var timeButton = document.createElement("button");
+        timeButton.className = "control-time";
+        timeButton.innerHTML = 'START';
+        timeButton.onclick = function(){controlTime(this, newTask);};
+
+        if (time == 0 && isNoTaskStarted() == false) {
+            timeButton.disabled = true;
+        }
+
+        li.appendChild(timeButton);
+        var elapsedTime = document.createElement("span");
+        elapsedTime.className = "elapsed-time";
+        // figure out what clock time is
+        var clockTime = convertToClockTime(time);
+        elapsedTime.appendChild(document.createTextNode(clockTime));
+        li.appendChild(elapsedTime);
+        return li;
+    }
+
+    var restoreList = function() {
+        if (supportsHtml5Storage()) {
+            if (localStorage.length > 0) {
+                var ul = document.getElementById("tasks");
+                for (key in localStorage) {
+                  ul.appendChild(createListItem(key, parseInt(localStorage[key])));
+                  tasks[key] = parseInt(localStorage[key]);
+                }
+            }
+        }
+    }
+
+    var setListItem = function (key, value) {
+        if (supportsHtml5Storage()) {
+            localStorage[key] = value;
+        }
+    }
+
+    var deleteListItem = function (key) {
+        if (supportsHtml5Storage()) {
+            localStorage.removeItem(key);
+        }
+    }
+
+    var deleteTask = function(deleteButton) {
+        var task = deleteButton.parentNode;
+        var taskName = deleteButton.nextSibling.firstChild.nodeValue;
+        clearInterval(timerIntervalId);
+        deleteListItem(taskName);
+        delete tasks[taskName];
+        task.remove();
+    }
+
+    var incrementTimer = function(timeButton, task, initialTime, startTime) {
+        var elapsedTime = new Date();
+        var elapsedTimeValue = timeButton.nextSibling.firstChild;
+        //elapsedTime = Date.now() - startTime + initialTime; // initialTime is the problem
+        elapsedTime = Date.now() - startTime + initialTime;
+        tasks[task] = elapsedTime;
+        setListItem(task, elapsedTime);
+        elapsedTimeValue.nodeValue = convertToClockTime(elapsedTime);
     }
 
     // Disable/Enable all other buttons when a task has started/stopped.
@@ -53,23 +142,13 @@ window.onload = function(){
         var initialTime = tasks[task];
         var elapsedTime;
         if (timeButton.innerHTML == 'START') {
-            // Check if no other task has been started and start the clock.
-            var timeButtons = document.getElementsByClassName("control-time");
-            var startTimer = true;
-            for (var i=0; timeButtons[i]; i++) {
-                if (timeButtons[i].innerHTML == 'STOP') {
-                    startTimer = false;
-                    break;
-                }
-            }
-            if (startTimer == true) {
+            if (isNoTaskStarted() == true) {
                 timeButton.innerHTML = 'STOP';
                 disableButtons(true);
 
                 // Initiate the start time.
                 var startTime = new Date();
                 timerIntervalId = setInterval(function(){incrementTimer(timeButton, task, initialTime, startTime);}, 1000);
-
             }
         }
         else {
@@ -84,52 +163,33 @@ window.onload = function(){
         if (newTask) {
             for (var task in tasks) {
                 if (task == newTask) {
-                    alert("You have a duplicate task, please enter a new task.");
+                    displayErrorMessage("You have a duplicate task, please enter a new task.");
                     return false;
                 }
             }
             return true;
         }
         else {
-            alert(" Please enter a task. ");
+            displayErrorMessage(" Please enter a task. ");
             return false;
         }
     }
 
     var addTask = function() {
+        deleteErrorMessage();
         var newTask = document.getElementById("new-task").value;
 
         if (isTaskValid(newTask)) {
             var ul = document.getElementById("tasks");
-            // Create a list element for the new task.
-            var li = document.createElement("li");
-            var deleteButton = document.createElement("button");
-            deleteButton.className = "delete-task";
-            deleteButton.innerHTML = 'x';
-            deleteButton.onclick = function(){deleteTask(this);};
-            li.appendChild(deleteButton);
-            var taskName = document.createElement("div");
-            taskName.className = "task-name";
-            taskName.innerHTML = newTask;
-            li.appendChild(taskName);
-            var timeButton = document.createElement("button");
-            timeButton.className = "control-time";
-            timeButton.innerHTML = 'START';
-            timeButton.onclick = function(){controlTime(this, newTask);};
-            li.appendChild(timeButton);
-            var elapsedTime = document.createElement("span");
-            elapsedTime.className = "elapsed-time";
-            elapsedTime.appendChild(document.createTextNode('00:00:00'));
-            li.appendChild(elapsedTime);
-            // Append the li element to the list of tasks.
-            ul.appendChild(li);
-
-            // Add that task to the tasks variable.
+            ul.appendChild(createListItem(newTask));
             tasks[newTask] = 0;
+            setListItem(newTask, 0);
         }
         // Clear the task input.
         document.getElementById("new-task").value = null;
     }
 
+    restoreList();
     document.getElementById("add-task").onclick = function() {addTask();};
+
 };
